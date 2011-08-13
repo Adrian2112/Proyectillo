760111f8de6fc9ee0d589c3404b4fd422b348702
Adrian Gonzalez <bernardogzzf@gmail.com>
2011-08-13 14:50:31 -0500

    creacion del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calmecac::Application.config.secret_token = '4f127581c9954b7989e9177b7e6e697484cb483596e86a57af785c15d23f4c8fefee1f0df4aae0702f3c4b932b109dc604148b33c5ebc5c98f21729755b71c30'
