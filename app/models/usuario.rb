3771e93eae07cb2db4f0fdbe8b513c4a43cac106
edo <elopez.deleon@gmail.com>
2011-10-18 14:49:17 -0500

    linkeados archivos existentes de modelo usuario para devise

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -13,6 +13,13 @@
 #
 
 class Usuario < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   attr_accessible :username, :email, :password, :password_confirmation, :campus_id, :universidad_id
   
