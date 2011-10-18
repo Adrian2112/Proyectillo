b0950dbac18050c21b300619cdb70cc4e5058455
edo <elopez.deleon@gmail.com>
2011-10-18 14:44:34 -0500

    Agregados y validados requisitos de devise en aplicacion

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Calmecac::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # For devise usage - Change the url for the host on deploy
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
