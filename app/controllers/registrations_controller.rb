a08520331c6e2ca3ccac946c5b90c151d60f0eb2
Adrian Gonzalez <bernardogzzf@gmail.com>
2011-10-23 02:00:46 -0500

    se arreglo los campos de tokeninput en registro

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     super
+    
     session[:omniauth] = nil unless @usuario.new_record?
+  end
+  
+  def edit
+    @universidad = @usuario.universidad
+    @universidad_data = @universidad.nil? ? "" : [{:id => @universidad.id, :nombre => @universidad.nombre}].to_json
+    @campus_url = @universidad ? "/universidades/#{@universidad.id}/campus" : ""
+    
+    @campus = @usuario.campus
+    @campus_data = @campus.nil? ? "" : [{:id => @campus.id, :nombre => @campus.nombre }].to_json
   end
   
   private
@@ -12,15 +22,6 @@ class RegistrationsController < Devise::RegistrationsController
       @usuario.apply_omniauth(session[:omniauth])
       @usuario.valid?
     end
-  end
-  
-  def edit
-    @universidad = @usuario.universidad
-    @universidad_data = @universidad.nil? ? "" : [{:id => @universidad.id, :nombre => @universidad.nombre}].to_json
-    @campus_url = @universidad ? "/universidades/#{@universidad.id}/campus" : ""
-    
-    @campus = @usuario.campus
-    @campus_data = @campus.nil? ? "" : [{:id => @campus.id, :nombre => @campus.nombre }].to_json
   end
   
 end
