4c63f82a68dd225a6193d7dbdffbb8cee01d3398
Abraham <abraham.kuri@gmail.com>
2011-10-23 02:19:49 -0500

    redireccion amistosa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
+  helper_method :store_requested_url, :clear_requested_url
+
   def current_user
     current_usuario
   end
+
+  def store_requested_url
+    session[:url] = request.url
+  end
   
+  def clear_requested_url
+    session[:url] = nil
+  end
+  
+  def after_sign_in_path_for(resource_or_scope)
+    session[:url] || root_path
+  end
+  
+  def after_sign_up_path_for(resource_or_scope)
+    session[:url] || root_path
+  end
+
 end
