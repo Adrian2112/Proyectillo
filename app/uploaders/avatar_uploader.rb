93e9661eae222279671397139359d4b5a51c18fa
Rafael Cárdenas <cardenas.rfl@gmail.com>
2011-10-25 21:30:55 -0500

    styles

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,7 +31,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_limit => [100, 100]
+     process :resize_to_limit => [108, 108]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
