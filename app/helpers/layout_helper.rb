b861f0a854b7462f9518d7658f136f8e56da01fe
edo <elopez.deleon@gmail.com>
2011-10-31 22:56:57 -0600

    agregada busqueda en todo el sitio

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -19,4 +19,9 @@ module LayoutHelper
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
+
+  def root
+    params[:controller] == 'pages' and params[:action] == 'index'
+  end
+
 end
