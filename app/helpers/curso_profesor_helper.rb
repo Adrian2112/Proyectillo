8dc894500ba4df278d6de0aa1cfb63211afa46c1
Abraham <abraham.kuri@gmail.com>
2011-11-01 03:45:12 -0600

    mensajes flash arreglados

--- app/helpers/curso_profesor_helper.rb
+++ app/helpers/curso_profesor_helper.rb
@@ -4,19 +4,23 @@ module CursoProfesorHelper
     if current_usuario.likes.where(:calificacion_id => calificacion.id).first
       link_to "", like_calificacion_path(calificacion), 
                   :remote => true, :method => :post, 
-                  :id => "like_link_#{calificacion.id}", :class => "unlike", :title => "No me gusta"
+                  :id => "like_link_#{calificacion.id}", :class => "unlike tooltip", :title => "No me gusta"
     else
 			link_to "", like_calificacion_path(calificacion), 
 			            :remote => true, :method => :post, 
-			            :id => "like_link_#{calificacion.id}", :class => "like", :title => "Me gusta"
+			            :id => "like_link_#{calificacion.id}", :class => "like tooltip", :title => "Me gusta"
 		end 
   end
   
   def display_flag_link(calificacion)
     if current_usuario.flags.where(:calificacion_id => calificacion.id).first
-			link_to "", flag_calificacion_path(calificacion), :remote => true, :method => :post, :id => "flag_link_#{calificacion.id}", :class => "unflag"
+			link_to "", flag_calificacion_path(calificacion), 
+			            :remote => true, :method => :post, 
+			            :id => "flag_link_#{calificacion.id}", :class => "unflag tooltip", :title => "Marcar como inapropiado"
 		else
-			link_to "", flag_calificacion_path(calificacion), :remote => true, :method => :post, :id => "flag_link_#{calificacion.id}", :class => "flag"
+			link_to "", flag_calificacion_path(calificacion), 
+			            :remote => true, :method => :post, 
+			            :id => "flag_link_#{calificacion.id}", :class => "flag tooltip", :title => "Desmarcar como inapropiado"
 		end
   end
   
