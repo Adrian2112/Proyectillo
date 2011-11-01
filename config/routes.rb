db74c323737ccad8c252a0552353f107f08563e4
edo <elopez.deleon@gmail.com>
2011-10-31 20:02:49 -0600

    url de invitacion masiva comentada en rutas

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,9 @@ Calmecac::Application.routes.draw do
   match 'contacto' => 'contact#new', :as => 'contact', :via => :get
   match 'contacto' => 'contact#create', :as => 'contact', :via => :post
   
-  match 'invitar' => 'invitation#new', :as => 'invitation', :via => :get
-  match 'invitar' => 'invitation#create', :as => 'invitation', :via => :post
+  # Para invitacion masiva de contactos. Implementacion no habilitada a los usuarios. 
+  #match 'invitar' => 'invitation#new', :as => 'invitation', :via => :get
+  #match 'invitar' => 'invitation#create', :as => 'invitation', :via => :post
   
 
   get 'pages/mas_resultados'
