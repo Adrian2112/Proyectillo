a2e989a896f3c6e2243482588292d18eb5ae537e
Adrian Gonzalez <bernardogzzf@gmail.com>
2011-11-03 22:12:58 -0600

    se validaron que las metricas esten en un rango de 1 a 10 y la calificacion obtenida de 0 a 100

--- app/models/calificacion.rb
+++ app/models/calificacion.rb
@@ -30,8 +30,9 @@ class Calificacion < ActiveRecord::Base
   
   before_save :calcula_promedio
 
-  CALIFAS = [0,1,2,3,4,5,6,7,8,9,10]
-
+  validates_inclusion_of :puntualidad, :amigable, :conocimiento, :claridad, :flexibilidad, :facilidad, :in => (1..10)
+  validates_inclusion_of :calificacion_obtenida, :in => (0..100)
+  
   default_scope order('likes_count DESC')
   default_scope order('created_at DESC')
   
