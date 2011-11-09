e953f475e11c7c94b36a534f07cecc301ed8bacf
Adrian Gonzalez <bernardogzzf@gmail.com>
2011-11-09 13:06:19 -0600

    se movieron los helpers de like_link y flag_link a calificaciones_helpers y se corrigio un error que habia en los helpers

--- app/helpers/calificaciones_helper.rb
+++ app/helpers/calificaciones_helper.rb
@@ -8,4 +8,38 @@ module CalificacionesHelper
     radio_buttons.html_safe
   end
   
+  def display_like_link(calificacion)
+    if current_usuario
+      if current_usuario.already_liked?(calificacion.id)
+        link calificacion, :tipo => "like", :class => "unlike",
+                            :title => "Ya no me gusta", :url => like_calificacion_path(calificacion)
+      else
+        link calificacion, :tipo => "like", :class => "like", 
+                            :title => "Me gusta", :url => like_calificacion_path(calificacion)
+      end
+    else
+  		  content_tag(:p, "", :class => "like", :tipo => "like")
+    end
+  end
+  
+  def display_flag_link(calificacion)
+    if current_usuario
+      if current_usuario.already_flagged?(calificacion.id)
+        link calificacion, :tipo => 'flag', :class => "unflag",
+                            :title => "Marcar como inapropiado", :url => flag_calificacion_path(calificacion)
+      else
+        link calificacion, :tipo => 'flag', :class => "flag",
+                            :title => "Desmarcar como inapropiado", :url => flag_calificacion_path(calificacion)
+      end
+    else
+      content_tag(:p, "", :class => "flag", :tipo => "flag")
+    end
+  end
+  
+  def link(calificacion, params={})
+     link_to "", params[:url], 
+  		            :remote => true, :method => :post, 
+  		            :id => "#{params[:tipo]}_link_#{calificacion.id}", :class => "#{params[:class]}", :title => "#{params[:title]}"
+  end
+  
 end
--- app/helpers/curso_profesor_helper.rb
+++ app/helpers/curso_profesor_helper.rb
@@ -1,33 +1,3 @@
 module CursoProfesorHelper
   
-  def display_like_link(calificacion)
-    if current_usuario
-      if current_usuario.already_liked?(calificacion.id)
-        link calificacion, :class => "unlike", :title => "No me gusta", :url => like_calificacion_path(calificacion)
-      else
-        link calificacion, :class => "like", :title => "Me gusta", :url => like_calificacion_path(calificacion)
-      end
-    else
-  		  content_tag(:p, "", :class => "like")
-    end
-  end
-  
-  def display_flag_link(calificacion)
-    if current_usuario
-      if current_usuario.already_flagged?(calificacion.id)
-        link calificacion, :class => "unflag", :title => "Marcar como inapropiado", :url => flag_calificacion_path(calificacion)
-      else
-        link calificacion, :class => "flag", :title => "Desmarcar como inapropiado", :url => flag_calificacion_path(calificacion)
-      end
-    else
-      content_tag(:p, "", :class => "flag")
-    end
-  end
-  
-  def link(calificacion, params={})
-     link_to "", params[:url], 
-  		            :remote => true, :method => :post, 
-  		            :id => "flag_link_#{calificacion.id}", :class => "#{params[:class]} tooltip", :title => "#{params[:title]}"
-  end
-  
 end
